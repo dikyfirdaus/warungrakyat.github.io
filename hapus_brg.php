284b67be979cbe21d0421211448e800ed07075b5
dikyfirdaus <dikyamik@gmial.com>
2023-05-09 16:05:27 +0700

    first commit

--- hapus_brg.php
+++
@@ -1,3 +0,0 @@
-<a href="p_brg.php">KEMBALI</a>
-<br/>
-<a href="hapus_aksi1.php">Yakin Hapus</a>
--- koneksidbs.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$koneksi = mysqli_connect("localhost","root","","smartshop_1");
- 
-// Check connection
-if (mysqli_connect_error()){
-	echo "Koneksi database gagal : " . mysqli_connect_error();
-}
- 
-?>
